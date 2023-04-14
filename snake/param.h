5dee56dbcac2b96fd745f6ed7dd9de900f5faf74
Reshetov Ivan <reshetov.prog@gmail.com>
2023-04-14 19:44:49 +0300

    start snake

---
+++ snake/param.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#define random(a,b) a+rand()%(b+1-a)
+
+const int SIZE_FIELD = 10;
+const int MARGIN = 5;
+const int MIN_SIZE_SNAKE = 3;
